61befac01b51fb34b736b7881dd2b5b002a3d89d
Moh Noor Al Azam <noor.azam@narotama.ac.id>
2022-08-31 11:10:36 +0700

    change ini file and create an .avi file output

--- cctvread.py
+++ cctvread.py
@@ -4,40 +4,57 @@ import cv2
 import numpy as np
 from configobj import ConfigObj
 import logging
-from time import sleep
+from time import sleep, time
 from datetime import datetime
+import argparse
 
-conf = ConfigObj("cctvread.ini")
-logging.basicConfig(filename=conf['main']['logname'], format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
+def parse_opt():
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--config', type=str, required=True, help='config file of the CCTV')
+	opt = parser.parse_args()
+	return(opt)
 
-logger = logging.getLogger('cctv-1')
-logger.setLevel(logging.DEBUG)
+def main(opt):
+	conf = ConfigObj(opt.config)
 
-logger.info('program starting')
+	logging.basicConfig(filename=conf['main']['logname'], format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
+	logger = logging.getLogger(conf['main']['cctv_name'])
+	logger.setLevel(logging.DEBUG)
+	logger.info('=========== PROGRAM STARTING ==========')
 
-while True:
-	cap = cv2.VideoCapture(conf['main']['source'])
-	if cap is None or not cap.isOpened():
-		logger.error('Unable to open video source: %s' % (conf['main']['source']))
-	else:
-		v_fps = cap.get(cv2.CAP_PROP_FPS)
-		v_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-		v_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-	
-		logger.info('Camera source opened : %s with %d x %d @ %d fps' % (conf['main']['source'], v_height, v_width, v_fps))
-		while True:
-		#for i in range(20):
-			ret, img = cap.read()
-			if ret:
-				fn = '%s/%s%s.jpg' % (conf['main']['output_path'], conf['main']['filename_prefix'], datetime.now().strftime("%Y%m%d%H%M%S%f"))
-				cv2.imwrite(fn, img)
-				logger.info('Image %s saved' % (fn))
-				sleep(1)
-				cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
-			else:
-				logger.error('Unable read video from source: %s' % (conf['main']['source']))
-				break
-		cap.release()
-	sleep(20)
+	while True:
+		cap = cv2.VideoCapture(conf['main']['source'])
+		if cap is None or not cap.isOpened():
+			logger.error('Unable to open video source: %s' % (conf['main']['source']))
+		else:
+			v_fps = cap.get(cv2.CAP_PROP_FPS)
+			v_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+			v_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+		
+			logger.info('Camera source opened : %s with %d x %d @ %d fps' % (conf['main']['source'], v_height, v_width, v_fps))
 
-logger.info("program ending")
+			fourcc = cv2.VideoWriter_fourcc(*'XVID')
+			fn = '%s/%s-%s.avi' % (conf['main']['output_path'], conf['main']['cctv_name'], datetime.now().strftime("%Y%m%d%H%M%S"))
+			out = cv2.VideoWriter(fn, fourcc, v_fps, (v_width,v_height))
+
+			end = start = time()
+			logger.info('Starting record video %s' % (fn))
+			while (end - start) <= float(conf['main']['video_length']):
+				ret, img = cap.read()
+				end = time()
+				if ret:
+					#img = cv2.flip(img, 0)
+					out.write(img)
+					#print("%f save to %s" % (end, fn))
+					sleep(1 / v_fps)
+				else:
+					logger.error('Unable read video from source: %s' % (conf['main']['source']))
+					break
+			out.release()
+			cap.release()
+			logger.info('Ending record video %s' % (fn))
+
+if __name__ == "__main__":
+	opt = parse_opt()
+	main(opt)
+	print("END-OF-PROGRAM")
